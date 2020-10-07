d148ba585742e6313d00b0b7597b677dd24fc92c
Elliu <goyard.louis@gmail.com>
2020-10-07 17:17:21 +0200

    Adding users, still got issues with the database

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInitd1521d9db2005bf469b6c7156457ab48
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
