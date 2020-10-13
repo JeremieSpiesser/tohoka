9a331c6f229b9a04a8cead7df4217beeb0fd1d98
Elliu <goyard.louis@gmail.com>
2020-10-13 19:54:41 +0200

    Creating the quizz table

---
+++ database/migrations/2020_10_13_192358_create_quizzs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuizzsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quizzs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->default("Quizz");
+            $table->json('content');
+            $table->unsignedBigInteger('creator');
+            $table->foreign('creator')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quizzs');
+    }
+}
