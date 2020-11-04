410e200878b99a045f94b757ee4ebb3b27e333ef
lec <alec.sadler@ensiie.fr>
2020-11-04 16:36:32 +0100

    added new page for modifying quizz, accessed via myquizz.blade.php

---
+++ resources/views/modifyquizz.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('custom-js')
+    <script type="application/javascript" src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script type="application/javascript" src="{{ asset('InputQuizz.js') }}"></script>
+@endsection
+@section('content')
+
+<?php $id=$_GET['quizzId']; 
+$cont=DB::select('select content from quizzs where id = :id',['id'=>$id]);
+$quizzContent=$cont[0]->content;
+echo "<p> $id</p>";
+echo "<p> $quizzContent</p>";
+?>
+
+<form method="POST" action="/modQuizz">
+    @csrf
+    <div class="container">
+        <div id="vue-app">
+            <h1>New Quizz  : @{{ quizz.getTitle() }} </h1>
+            <input type="textbox" name="name" v-model="quizz.title" />
+            <ul>
+                <li v-for="item in quizz.items">
+                    <h3>@{{ item.getQuestion() }} </h3>
+                    <input type="textbox" v-model="item.question" />
+
+
+                    <ul v-for="(possAnswer,index) in item.getAnswers()">
+                        <li>  @{{ index }} : @{{ possAnswer.answer }} </li> : <input type="textbox" v-model="possAnswer.answer" />
+                        <input type="checkbox" v-model="possAnswer.bool" placeholder="Correct answer ?" />
+                    </ul>
+                    <button type="button" @click="item.addPossibleAnswer()"> Add an answer </button>
+                </li>
+            </ul>
+            <button type="button" v-on:click="quizz.addItem()"> Add item </button>
+            <button type="button" v-on:click="populateJson()" >Export to JSON </button>
+            <input v-model="jsonExport" name="content" hidden="yes">
+            <p> @{{ jsonExport }}</p>
+            <input v-on:click="populateJson()" type="submit" value="Save this quizz."></input>
+        </div>
+    </div>
+</form>
+
+@endsection
