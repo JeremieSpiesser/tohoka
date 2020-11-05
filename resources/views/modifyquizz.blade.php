d5af378f3058ed48c460fdd60ff0b9f4c1bf159f
Jérem SP <jeremie.spiesser@gmail.com>
2020-11-05 04:10:46 +0100

    updated modifyquizz so that we can modify custom quizzes

--- resources/views/modifyquizz.blade.php
+++ resources/views/modifyquizz.blade.php
@@ -20,20 +20,41 @@ echo "<script type='application/javascript' defer> var chaine = '$quizzContent' 
             <h1>New Quizz  : @{{ quizz.getTitle() }} </h1>
             <input type="textbox" name="name" v-model="quizz.title" />
             <ul>
-                <li v-for="item in quizz.items">
-                    <h3>@{{ item.getQuestion() }} </h3>
-                    <input type="textbox" v-model="item.question" />
+             <li v-for="item in quizz.items">
+                 <h3>@{{ item.getQuestion() }} </h3>
+                 <input type="textbox" v-model="item.question" />
+                 <select value="qcm" v-model="item.type">
+                     <option value="qcm">QCM sans avertissement</option>
+                     <option value="classic">Question à réponse unique</option>
+                     <option value="qcma">QCM avec avertissement</option>
+                     <option value="tf">Vrai/Faux</option>
+                 </select>
 
+                 <div v-if="item.type === 'classic'">
+                 <ul v-for="(possAnswer,index) in item.getAnswers()">
+                     <li>  @{{ index }} : @{{ possAnswer.answer }} </li> : <input type="textbox" v-model="possAnswer.answer" />
+                      Vrai ? <input type="checkbox" v-model="possAnswer.bool" @click="item.disableOthers(possAnswer)" placeholder="Correct answer ?" />
+                 </ul>
+                 <button type="button" @click="item.addPossibleAnswer()"> Add an answer </button>
 
-                    <ul v-for="(possAnswer,index) in item.getAnswers()">
-                        <li>  @{{ index }} : @{{ possAnswer.answer }} </li> : <input type="textbox" v-model="possAnswer.answer" />
-                        <input type="checkbox" v-model="possAnswer.bool" placeholder="Correct answer ?" />
-                    </ul>
-                    <button type="button" @click="item.addPossibleAnswer()"> Add an answer </button>
-                </li>
-            </ul>
-            <button type="button" v-on:click="quizz.addItem()"> Add item </button>
-            <button type="button" v-on:click="populateJson()" >Export to JSON </button>
+                 </div>
+
+                 <div v-if="item.type === 'tf'">
+                     <input type="radio" value=true v-model="item.isTrue">Vrai</input>
+                     <input type="radio" value=false v-model="item.isTrue">Faux</input>
+                 </div>
+                 <div v-if="item.type.includes('qcm')">
+                 <ul v-for="(possAnswer,index) in item.getAnswers()">
+                     <li>  @{{ index }} : @{{ possAnswer.answer }} </li> : <input type="textbox" v-model="possAnswer.answer" />
+                      Vrai ? <input type="checkbox" v-model="possAnswer.bool" placeholder="Correct answer ?" />
+                 </ul>
+                 <button type="button" @click="item.addPossibleAnswer()"> Add an answer </button>
+                 </div>
+             </li>
+         </ul>
+         <button type="button" v-on:click="quizz.addItem()"> Add item </button>
+         <button type="button" v-on:click="populateJson()" >Export to JSON </button>
+
             <input v-model="jsonExport" name="content" hidden="yes">
             <?php echo "<input type='hidden' name='id' value=$id />" ?>
             <p> @{{ jsonExport }}</p>
