72990775e3345a3e937acdb6a789b54d3859986a
akemi <wacorredorm@unal.edu.co>
2020-11-26 02:51:51 +0100

    :sparkles: Minor styling details.

--- resources/views/myquizz.blade.php
+++ resources/views/myquizz.blade.php
@@ -2,22 +2,22 @@
 
 @section('content')
     <div class="container">
-    <h1>My quizz :</h1>
+    <h1 class="display-3">My quizzes:</h1>
         @forelse ($quizzs as $quizz)
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">{{ $quizz->name }}</h5>
+                    <h5  class="card-title">{{ $quizz->name }}</h5>
                     <p class="card-text">{{ $quizz->content }}</p>
 
-                    <a class="btn btn-success" href="{{ route('quizz-play', ['id' => $quizz->id]) }}">
+                    <a class="btn btn-outline-success" href="{{ route('quizz-play', ['id' => $quizz->id]) }}">
                         Play this quizz
                     </a>
 
-                    <a class="btn btn-primary" href="{{ route('quizz-modify', ['id' => $quizz->id]) }}">
-                        Modify this quizz
+                    <a class="btn btn-outline-info" href="{{ route('quizz-modify', ['id' => $quizz->id]) }}">
+                        Edit this quizz
                     </a>
 
-                    <a class="btn btn-danger" href="{{ route('quizz-api-drop', ['id' => $quizz->id]) }}">
+                    <a class="btn btn-outline-danger" href="{{ route('quizz-api-drop', ['id' => $quizz->id]) }}">
                         Delete this quizz
                     </a>
                 </div>
@@ -25,7 +25,7 @@
 
         @empty
             <div class="alert alert-warning" role="alert">
-                You have no quizz yet. <a href="{{ route('quizz-create') }}" class="alert-link">Create one ?</a>
+                You have no quizzes yet. <a href="{{ route('quizz-create') }}" class="alert-link"> Would you like to create one ?</a>
             </div>
 
         @endforelse
