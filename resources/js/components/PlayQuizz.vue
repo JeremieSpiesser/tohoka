798afa3d60ecd7287b84a61a6f35a750ee274f84
Nitorac <nitorac.r@gmail.com>
2020-11-26 04:07:21 +0100

    Hide Play BG music if not available

--- resources/js/components/PlayQuizz.vue
+++ resources/js/components/PlayQuizz.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>{{ quizz.title }}</h1>
-        <button @click="toggleBGM()" type="button">Play background audio</button>
+        <button v-if="quizzBgm" @click="toggleBGM()" type="button">Play background audio</button>
         <ul>
             <li v-for="item in quizz.items">
                 <h3  v-if="gameFinished" class="alert alert-primary" role="alert">
