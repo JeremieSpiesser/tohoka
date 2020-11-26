023f9f2cb4a25c9d77e4472f8ff11dc408105b5e
akemi <wacorredorm@unal.edu.co>
2020-11-26 02:36:14 +0100

    :sparkles: Changed edit quiz style

--- resources/js/components/EditQuizz.vue
+++ resources/js/components/EditQuizz.vue
@@ -1,30 +1,38 @@
 <template>
-    <div>
-        <h1>Quizz : {{ quizz.title }}</h1>
-        <input type="textbox" name="name" v-model="quizz.title" />
-        <ul>
-            <li v-for="(item,ind) in quizz.items">
-                <h3>{{ item.question }} </h3>
-                <input type="textbox" v-model="item.question" />
-                <input type="button" value="Delete" 			@click="quizz.removeItemAt(ind)">
-          
-                
-                <select v-model="item.type">
-                    <option value="qcm">QCM sans avertissement</option>
-                    <option value="classic">Question à réponse unique</option>
-                    <option value="qcma">QCM avec avertissement</option>
-                    <option value="tf">Vrai/Faux</option>
-                </select>
+    <div> 
+        <head>
+            <link rel = "stylesheet" href = "~/Desktop/ENSIIE/PIMA/project/pima-groupe4-2020-tohoka/resources/sass/app.scss">
+        </head>
 
+        
+        <h1 class="display-2" id = "Head">Quizz: {{ quizz.title }}</h1>
+        <input type="textbox" name="name" id ="QuizTitle" placeholder="Nom du Quizz" v-model="quizz.title" />
+        <ul>
+            <li id="Questionnaire" v-for="(item,ind) in quizz.items">
+                
+                <h3>{{ item.question }} </h3>
+                
+                <form class="form-inline">
+                <div class="form-row align-items-center">
+                    <input type="textbox" v-model="item.question" />            
+                    <input type="button"  class="btn btn-outline-danger" value="Delete." 	@click="quizz.removeItemAt(ind)">            
+                    <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
+                    <select class="custom-select mr-sm-2" id="inlineFormCustomSelect" v-model="item.type">
+                        <option value="qcm">QCM sans avertissement</option>
+                        <option value="classic">Question à réponse unique</option>
+                        <option value="qcma">QCM avec avertissement</option>
+                        <option value="tf">Vrai/Faux</option>
+                    </select>
+                </div>
+                </form>
                 <div v-if="item.type === 'classic'">
                     <ul v-for="(possAnswer,index) in item.answers">
               
                         <li>  {{ index }} : {{ possAnswer.answer }} </li> : <input type="textbox" v-model="possAnswer.answer" /> 
-                        <input type="button" value="Delete" @click="item.removeItemAt(index)"/>
+                        <input type="button"  class="btn btn-outline-danger btn-sm"value="Delete" @click="item.removeItemAt(index)"/>
                         Vrai ? <input type="checkbox" v-model="possAnswer.bool" @click="item.disableOthers(possAnswer)" placeholder="Correct answer ?" />  
-		</ul>
-                    <button type="button" @click="item.addPossibleAnswer()"> Add an answer </button>
-
+		            </ul>
+                    <button type="button"  class="btn btn-outline-success btn-sm" @click="item.addPossibleAnswer()" > Add an answer </button>
                 </div>
 
                 <div v-if="item.type === 'tf'">
@@ -34,19 +42,19 @@
                 <div v-if="item.type.includes('qcm')">
                     <ul v-for="(possAnswer,index) in item.answers">
                         <li>  {{ index }} : {{ possAnswer.answer }} </li> : <input type="textbox" v-model="possAnswer.answer" />
-                                                   <input type="button" value="Delete" @click="item.removeItemAt(index)"/>
+                                                   <input type="button" class="btn btn-outline-danger btn-sm" data-toggle="button" aria-pressed="false" value="Delete" @click="item.removeItemAt(index)"/>
                         Vrai ? <input type="checkbox" v-model="possAnswer.bool" placeholder="Correct answer ?" />
 
                     </ul>
-                    <button type="button" @click="item.addPossibleAnswer()"> Add an answer </button>
+                    <button type="button" class="btn btn-outline-success "  @click="item.addPossibleAnswer()"> Add an answer </button>
                 </div>
             </li>
         </ul>
-        <button type="button" v-on:click="quizz.addItem()"> Add item </button>
-        <button type="button" v-on:click="populateJson()" >Export to JSON </button>
-        <input v-model="jsonExport" name="content" hidden="yes" />
+        <button type="button" class="btn btn-outline-info" aria-pressed="false "  v-on:click="quizz.addItem()"> Add item </button>
+        <button type="button" class="btn btn-outline-info" data-toggle="button " aria-pressed="false" v-on:click="populateJson()" >Export to JSON </button>
+        <input v-model="jsonExport" name="content" hidden="yes" >
         <p> {{ jsonExport }}</p>
-        <input v-on:click="populateJson()" type="submit" value="Save this quizz."/>
+        <input class="btn btn-secondary" v-on:click="populateJson()" type="submit" value="Save this quizz."/>
     </div>
 </template>
 
@@ -59,7 +67,7 @@ export default {
     props: ['quizzContent'],
     data: function () {
         return {
-            quizz: new InputQuizz("Sample quizz"),
+            quizz: new InputQuizz("Sample Quiz."),
             jsonExport: ""
         }
     },
@@ -81,4 +89,6 @@ export default {
         }
     }
 }
+
+
 </script>
