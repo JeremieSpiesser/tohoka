9fdabcb0996bfb66c1c8048ebcd3a78d2a09aad1
Elliu <goyard.louis@gmail.com>
2020-11-27 17:30:28 +0100

    Now we can load the question (still no variable and navbar and all also loaded, need fix)

--- resources/js/components/PlayQuizz.vue
+++ resources/js/components/PlayQuizz.vue
@@ -9,74 +9,9 @@
                 Pause background audio
             </span>
         </button>
-        <ul>
-            <li v-for="item in quizz.items">
-                <h3  v-if="gameFinished" class="alert alert-primary" role="alert">
-                    {{ item.question }}<div style="text-align:right;font-style: italic;">Score : {{ item.score }}/{{ item.getNbGoodAnswers() }}</div></h3>
-                <h3  v-else>{{ item.question }} </h3>
-                <div v-if='item.type.includes("qcm")'>
-                    <h4 v-if="item.type === 'qcma' && !gameFinished">Multiple answers are possible here !</h4>
-                    <ul v-for="(possAnswer,index) in item.answers">
-                        <li>{{ index }} : {{ possAnswer.answer }}
-                            <input type="checkbox" v-model="possAnswer.userChoice">
-                            <div v-if="gameFinished">
-                                <p v-if="possAnswer.userChoice && (possAnswer.userChoice === possAnswer.bool)" style="color:green"> Bravo :) </p>
-
-                                <p v-else-if="possAnswer.userChoice && !(possAnswer.userChoice === possAnswer.bool)" style="color:red"> C'est FAUX :( </p>
-                                <p v-else-if="!possAnswer.userChoice && possAnswer.bool"> Il fallait cliquer ici, dommage :/ </p>
-
-                                <p v-if="!possAnswer.bool && !possAnswer.userChoice"></p>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-                <div v-if="item.type === 'classic'">
-                    <h4>Only one answer is possible here !</h4>
-                    <ul v-for="(possAnswer, index) in item.answers">
-                        <li>
-                            <!--input type="checkbox" v-model="possAnswer.userChoice" @click="item.disableOthers(possAnswer)"-->
-                            <input type="radio" v-model="item.userUniqueChoice" :value="possAnswer.answer">{{ possAnswer.answer }}</input>
-
-                            <div v-if="gameFinished">
-                                <!-- Si coché et ok -  nice
-                                sinon si pas coché et ok - not nice
-                                si coché et pas ok - not nice -->
-                                <p v-if="(item.userUniqueChoice === possAnswer.answer) && (possAnswer.bool)" style="color:green">Bravo :) </p>
-                                <p v-else-if="(item.userUniqueChoice === possAnswer.answer) && (!possAnswer.bool)" style="color:red"> Faux :( </p>
-                                <p v-if="(item.userUniqueChoice !== possAnswer.answer) && possAnswer.bool"> Il fallait cliquer ici, dommage :/</p>
-                            </div>
-                        </li>
-                    </ul>
-
-                </div>
-                <div v-if="item.type === 'tf'">
-                    <input type="radio" v-model="item.userTfChoice" value="true">Vrai</input>
-                    <input type="radio" v-model="item.userTfChoice" value="false">Faux</input>
-                    <div v-if="gameFinished">
-                        <p v-if="item.userTfChoice === item.isTrue" style="color:green">Bravo :)</p>
-                        <p v-if="item.userTfChoice !== item.isTrue" style="color:red">C'est Faux :(</p>
-                    </div>
-                </div>
-            </li>
-        </ul>
-
-        <button type="button" class="btn btn-primary" v-if="!gameFinished" @click="toggleFinished()"> Show answers </button>
-
-        <div class="jumbotron" v-if="gameFinished" style="background-color:#ffc107; color:white;">
-            <div class="container">
-                <h1 class="display-4" v-if="score === 100">Congratulations, you've got {{ nbpoints }}/{{ nbgoodanswers }} !</h1>
-                <h1 class="display-4" v-else-if="score > 75">Almost perfect, you've got {{ nbpoints }}/{{ nbgoodanswers }} !</h1>
-                <h1 class="display-4" v-else-if="score > 50">Quite good, you've got {{ nbpoints }}/{{ nbgoodanswers }} !</h1>
-                <h1 class="display-4" v-else-if="score > 25">You've got {{ nbpoints }}/{{ nbgoodanswers }}, you'll have to study a bit more ...</h1>
-                <h1 class="display-4" v-else>Uh-oh, you've only got {{ nbpoints }}/{{ nbgoodanswers }} ...</h1>
-                <div class="progress">
-                    <div id="achievement" class="progress-bar progress-bar-striped bg-success" role="progressbar" v-bind:style="{ width: score + '%' }" style="font-weight: bold" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">{{ score }}%</div>
-                </div><br/>
-                <hr>
-                <div class="row justify-content-center">
-                    <button type="button" class="d-flex justify-content-center btn btn-danger" @click="toggleFinished()"> Hide answers </button>
-                </div>
-            </div>
+        <button @click="loadQuestion(1,1)" type="button">Load question</button>
+        <div id="currentQuestion">
+            {{ question }}
         </div>
 
     </div>
@@ -98,7 +33,8 @@ export default {
                 nbpoints: 0,
                 score: 0,
                 audio: undefined,
-                isAudioPlaying: false
+                isAudioPlaying: false,
+                question: ""
             }
         },
         mounted() {
@@ -156,6 +92,14 @@ export default {
                 }
                 this.score = Math.round((this.nbpoints / this.nbgoodanswers) * 100);
                 //this.handleProgressBar();
+            },
+
+            loadQuestion(quizzId, questionId){
+                axios.get('/getquizzquestion/18,1')
+                    .then((response)=>{
+                        document.getElementById("currentQuestion").innerHTML = response.data;
+                        //this.question = response.data
+                    })
             }
         }
     }
--- resources/views/createquizz.blade.php
+++ resources/views/createquizz.blade.php
@@ -1,11 +1,10 @@
-@extends('layouts.appvuejs')
+@extends('layouts.loadablediv')
 
-@section('app')
+@section('content')
 <form method="POST" action="{{ route('quizz-api-save') }}" enctype="multipart/form-data">
     @csrf
     <div class="container">
         <edit-quizz></edit-quizz>
     </div>
 </form>
-
 @endsection
