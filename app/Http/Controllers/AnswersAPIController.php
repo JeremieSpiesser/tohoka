476e9344131ca5acd5b92dece4e6cd8705d32ca5
Elliu <goyard.louis@gmail.com>
2020-11-28 22:34:45 +0100

    Adding Instance/Answers model/Controllers

---
+++ app/Http/Controllers/AnswersAPIController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AnswersAPIController extends Controller
+{
+    public function registerPlayer(Request $request){
+
+    }
+}
---
+++ app/Models/Instance.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Instance extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'master',
+        'idQuizz',
+        'currentQuestion'
+    ];
+}
