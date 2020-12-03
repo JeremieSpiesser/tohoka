8039cd21958d5c5c875ef4e6e032907cdd859299
Elliu <goyard.louis@gmail.com>
2020-12-03 19:59:45 +0100

    Yeet the classes

--- resources/js/components/PlayQuizz.vue
+++ resources/js/components/PlayQuizz.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>{{ quizz.title }}</h1>
+        <h1>{{ quizzContent.title }}</h1>
         <button v-if="quizzBgm" @click="toggleBGM()" type="button">
             <span v-if="!isAudioPlaying">
                 Play background audio
@@ -20,7 +20,7 @@
 
 <script>
 
-import {OutputQuizz, OutputQuizzItem} from "../classes/outputQuizz";
+//import {OutputQuizz, OutputQuizzItem} from "../classes/outputQuizz";
 
 import Question from "../components/Question.vue";
 
@@ -33,7 +33,7 @@ export default {
         },
         data: function(){
             return {
-                quizz: new OutputQuizz(),
+                //quizz: new OutputQuizz(),
                 gameFinished: false,
                 nbgoodanswers: 0,
                 nbpoints: 0,
@@ -59,12 +59,13 @@ export default {
                 this.n = count;
                 this.quizz_Id = quizz_Id;
                 this.idInstance = idInstance;
+                this.quizzContent = JSON.parse(this.quizzContent);
                 console.log("Corresponding json :" + text);
-                Object.assign(this.quizz, JSON.parse(text));
-                this.quizz.items = this.quizz.items.map((item) => new OutputQuizzItem(item.question, item.answers, item.type));
-                this.quizz.items.forEach(function(item){
-                    item.userChoice = false;
-                });
+                //Object.assign(this.quizz, JSON.parse(text));
+                //this.quizz.items = this.quizz.items.map((item) => new OutputQuizzItem(item.question, item.answers, item.type));
+                //this.quizz.items.forEach(function(item){
+                //    item.userChoice = false;
+                //});
             },
             initBGM(path){
                 this.audio = new Audio(path);
@@ -79,29 +80,6 @@ export default {
                       this.isAudioPlaying = true;
                       this.audio.play();
                   }
-            },
-
-            /*handleProgressBar(){
-                var progress = document.getElementById("achievement");
-                progress.focus();
-                progress.style.width = this.score + "%";
-                //progress.aria-valuenow = this.score;
-
-            },*/
-            toggleFinished() {
-                console.log(this.nbpoints);
-                this.reset();
-                document.querySelectorAll('input[type=checkbox]').forEach((checkbox) => {
-                    checkbox.disabled = !checkbox.disabled;
-                })
-                this.gameFinished = !this.gameFinished;
-                for (const item of this.quizz.items) {
-                    item.computeScore();
-                    this.nbpoints += item.score;
-                    this.nbgoodanswers += item.getNbGoodAnswers();
-                }
-                this.score = Math.round((this.nbpoints / this.nbgoodanswers) * 100);
-                //this.handleProgressBar();
             },
 
             loadNextQuestion(){
