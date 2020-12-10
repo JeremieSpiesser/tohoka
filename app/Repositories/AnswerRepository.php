32ae431afdf994e9a80168802f542072459ef7a6
Elliu <goyard.louis@gmail.com>
2020-12-10 03:06:57 +0100

    Sending the result if beyond last questoin is asked to be open

--- app/Repositories/AnswerRepository.php
+++ app/Repositories/AnswerRepository.php
@@ -28,4 +28,29 @@ class AnswerRepository
             ->where('idPlayer', $idPlayer)
             ->update(['answers' => $newAnswer]);
     }
+
+    public static function countAnswerPoints($idInstance, $idPlayer){
+        $playerAnswer= json_decode(AnswerRepository::getPlayerAnswer($idInstance,$idPlayer),true);
+        $realQuestion = json_decode(DB::table('quizzs')
+            ->select('content')
+            ->where('id',InstanceRepository::getQuizzId($idInstance))
+            ->first()->content
+            ,true)->items;
+        
+        $points=0;
+
+        for ($i=0 ; i<count($realQuestion);i++){
+            $realAns = realQuestion[i]->answer;
+            $ans = playerAnswer[i];
+            $r=count(ans);
+            $a=count(realAns);
+            for ($j=0; $j<min($r,$a);j++){
+                if (realAns['bool'] && ans['bool']){
+                    $points = $points + 1;
+                }
+            }
+        }
+
+       return $points; 
+    }
 }
